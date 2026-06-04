d96395d00e7d51e92f0427dd1d2f28e06318efb7
Raafki <raafkieducation@gmail.com>
2026-06-04 10:44:04 +0700

    Fix: Explicitly require pg for Vercel bundler

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const cors    = require('cors');
 require('dotenv').config();
 
+// Force bundler to include pg (Sequelize loads it dynamically)
+require('pg');
+require('pg-hstore');
+
 const { connectDB } = require('./src/config/database');
 
 // Daftarkan semua model
